c3e9020ef02d16c8e4f261a58cb7836dcb525fe9
Zablon Charles <zablon.charles@gmail.com>
2024-04-04 15:13:03 -0500

    v15:2:1: new project folder

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,11 +1,11 @@
 var plugins = [{
-      plugin: require('/Users/zabloncharles/Desktop/Port-Project-React/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/zabloncharles/Portfolio/Portfolio-project/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     },{
-      plugin: require('/Users/zabloncharles/Desktop/Port-Project-React/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
+      plugin: require('/Users/zabloncharles/Portfolio/Portfolio-project/node_modules/gatsby-plugin-styled-components/gatsby-ssr'),
       options: {"plugins":[],"displayName":true,"fileName":true,"minify":true,"namespace":"","transpileTemplateLiterals":true,"pure":false},
     },{
-      plugin: require('/Users/zabloncharles/Desktop/Port-Project-React/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
+      plugin: require('/Users/zabloncharles/Portfolio/Portfolio-project/node_modules/gatsby-plugin-manifest/gatsby-ssr'),
       options: {"plugins":[],"name":"Fusion+Code","short_name":"FusionCode","start_url":"/","background_color":"#ffffff","theme_color":"#663399","display":"minimal-ui","icon":"static/icon.png","legacy":true,"theme_color_in_head":true,"cache_busting_mode":"query","crossOrigin":"anonymous","include_favicon":true,"cacheDigest":"5782ee6af923248f75d90a2be9f0f214"},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,11 +5,11 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/zabloncharles/Desktop/Port-Project-React/.cache/dev-404-page.js"))),
-  "component---src-pages-index-3-js": hot(preferDefault(require("/Users/zabloncharles/Desktop/Port-Project-React/src/pages/index-3.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/zabloncharles/Desktop/Port-Project-React/src/pages/index.js"))),
-  "component---src-pages-mobile-app-js": hot(preferDefault(require("/Users/zabloncharles/Desktop/Port-Project-React/src/pages/MobileApp.js"))),
-  "component---src-pages-web-app-js": hot(preferDefault(require("/Users/zabloncharles/Desktop/Port-Project-React/src/pages/WebApp.js"))),
-  "component---src-pages-welcome-typing-message-js": hot(preferDefault(require("/Users/zabloncharles/Desktop/Port-Project-React/src/pages/WelcomeTypingMessage.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/zabloncharles/Portfolio/Portfolio-project/.cache/dev-404-page.js"))),
+  "component---src-pages-index-3-js": hot(preferDefault(require("/Users/zabloncharles/Portfolio/Portfolio-project/src/pages/index-3.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/zabloncharles/Portfolio/Portfolio-project/src/pages/index.js"))),
+  "component---src-pages-mobile-app-js": hot(preferDefault(require("/Users/zabloncharles/Portfolio/Portfolio-project/src/pages/MobileApp.js"))),
+  "component---src-pages-web-app-js": hot(preferDefault(require("/Users/zabloncharles/Portfolio/Portfolio-project/src/pages/WebApp.js"))),
+  "component---src-pages-welcome-typing-message-js": hot(preferDefault(require("/Users/zabloncharles/Portfolio/Portfolio-project/src/pages/WelcomeTypingMessage.js")))
 }
 
